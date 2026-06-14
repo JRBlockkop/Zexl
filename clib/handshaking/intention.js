bdf7bba96ff95da6b1b8bcf380f916adfff20e89
JRBlockkop <161836282+JRBlockkop@users.noreply.github.com>
2026-06-14 22:25:34 +0200

    cli: changes

--- clib/handshaking/intention.js
+++ clib/handshaking/intention.js
@@ -1,7 +1,10 @@
+const {readVarInt} = require('./../../types/Data')
+
 const Intention = {
     id: 0x00,
     c: class{
         constructor(ProtocolVersion,ServerAddress,ServerPort,Intent){
+            this.id = Intention.id;
             this.ProtocolVersion = ProtocolVersion;
             this.ServerAddress = ServerAddress;
             this.ServerPort = ServerPort;
@@ -11,8 +14,15 @@ const Intention = {
             
         }
     },
-    f: function(){
-
+    f: function(hex){
+        let mainbody = String(hex);
+        let [ProtocolVersion,l1] = readVarInt(mainbody);
+        mainbody = mainbody.slice(l1*2);
+        let [v2,l2] = readVarInt(mainbody);
+        const ServerAddress =  Buffer.from(mainbody.slice(2*l2,2*(v2+l2)),'hex').toString();mainbody = mainbody.slice(2*(v2+l2))
+        const ServerPort = Number('0x'+mainbody.slice(0, 4 ));mainbody = mainbody.slice( 4 )
+        let [Intent,_] = readVarInt(mainbody)
+        return new this.c(ProtocolVersion,ServerAddress,ServerPort,Intent)
     }
 }
 
