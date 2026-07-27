53ed0f4fb2ea2725889f44df72981cea2ef7c9d9
JRBlockkop <161836282+JRBlockkop@users.noreply.github.com>
2026-07-27 17:10:21 +0200

    fixed StatusResponse timing

--- data/packetHandler.js
+++ data/packetHandler.js
@@ -29,54 +29,63 @@ function packetHandler(con,d,id){
     mypackets.forEach(myhex => {
         try{
             const packet = Packet.from(myhex)
-            console.log(packet.id);
-            if(ConnectionState.Handshaking == con.state){
-                HandshakingPackets[packet.id].f(packet.data);
-                if(packet.id == clib.handshaking.Intention.id){
-                    const JSONResponse = {
-                        version: {
-                            name: "Zexl-1.21.11",
-                            protocol: 774
-                        },
-                        players: {
-                            max: 20,
-                            online: 0,
-                            sample: []
-                        },
-                        description: {
-                            text: "Hello Zexl!"
-                        },
-                        favicon: "data:image/png;base64,<data>",
-                        enforcesSecureChat: false
+            console.log("[Zexl]","PacketId=",packet.id);
+            let data;
+            switch (con.state) {
+                case ConnectionState.Handshaking:
+                    data = HandshakingPackets[packet.id].f(packet.data);
+                    if(packet.id == clib.handshaking.Intention.id){
+                        if(data.Intent == 1){
+                            con.state = String(ConnectionState.Status);
+                        }
+                        if(data.Intent == 2){
+                            con.state = String(ConnectionState.Login);
+                        }
                     }
+                    break;
+                case ConnectionState.Status:
+                    data = StatusPackets[packet.id].f(packet.data);
+                    if(packet.id == clib.status.StatusRequest.id){
+                        const JSONResponse = {
+                            version: {
+                                name: "Zexl-1.21.11",
+                                protocol: 774
+                            },
+                            players: {
+                                max: 20,
+                                online: 0,
+                                sample: []
+                            },
+                            description: {
+                                text: "Hello Zexl!"
+                            },
+                            favicon: "data:image/png;base64,<data>",
+                            enforcesSecureChat: false
+                        }
 
-                    const [id,hex] = new slib.status.StatusResponse.c(JSON.stringify(JSONResponse)).toHex()
-                    
-                    con.send(   
-                        new Packet(
-                            id,
-                            hex
-                        ).toHex()
-                    )
+                        const [id,hex] = new slib.status.StatusResponse.c(JSON.stringify(JSONResponse)).toHex()
 
-                    con.state = String(ConnectionState.Status);
-                }
-            }
-            if(ConnectionState.Status == con.state){
-                const data = StatusPackets[packet.id].f(packet.data);
-                if(packet.id == clib.status.PingRequest.id){
+                        con.send(   
+                            new Packet(
+                                id,
+                                hex
+                            ).toHex()
+                        )
+                    }
+                    if(packet.id == clib.status.PingRequest.id){
+                        const [id,hex] = new slib.status.PingResponse.c(data.Timestamp).toHex()
 
-                    const [id,hex] = new slib.status.PingResponse.c(data.Timestamp).toHex()
-
-                    console.log(id.hex)
-
-                    con.send(
-                        new Packet(
-                            id,
-                            hex
-                        ).toHex()
-                    )
-                }
+                        con.send(
+                            new Packet(
+                                id,
+                                hex
+                            ).toHex()
+                        )
+                    }
+                    break;
+                case ConnectionState.Login:
+                    console.log("[Zexl]","LoginStart");
+                    break;
             }
         } catch(e){con.close();console.log(e)}
     });
